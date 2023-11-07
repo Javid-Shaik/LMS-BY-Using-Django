c49394fd78121ae79869b9f0cfb64199f63e9ace
Javid-Shaik <shaikjavid8640@gmail.com>
2023-11-07 14:34:40 +0530

    Ready for deployment

--- myenv/Scripts/LibraryManagementSystem/LibraryManagementSystem/settings.py
+++ myenv/Scripts/LibraryManagementSystem/LibraryManagementSystem/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = config('SECRET_KEY', default='secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".onrender.com"]
 
 
 # Application definition
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -133,6 +134,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR ,'static')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
